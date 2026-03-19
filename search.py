07e706319abb4072117299252fe5806c925a4d2b
Gagan K <kgagangk05@gmail.com>
2026-03-19 18:59:36 +0530

    updated

--- search.py
+++ search.py
@@ -3,13 +3,14 @@ import torch
 from PIL import Image
 from sklearn.metrics.pairwise import cosine_similarity
 from model import model, transform
+import matplotlib.pyplot as plt
 
 # Load saved data
 features = np.load("features.npy")
 paths = np.load("paths.npy")
 
 # Load query image
-query_path = "dataset/img_32.jpg"
+query_path = "dataset/img_31.jpg"
 
 image = Image.open(query_path).convert("RGB")
 image = transform(image).unsqueeze(0)
@@ -25,7 +26,25 @@ similarities = cosine_similarity([query_feature], features)[0]
 # Get top 5 matches
 top_indices = similarities.argsort()[-5:][::-1]
 
-print("Top similar images:")
 
-for i in top_indices:
-    print(paths[i])
+
+# Display query + results
+plt.figure(figsize=(10,4))
+
+# Show query image
+plt.subplot(1,6,1)
+plt.imshow(Image.open(query_path))
+plt.title("Query")
+plt.axis("off")
+
+# Show similar images
+for i, idx in enumerate(top_indices):
+    img = Image.open(paths[idx])
+
+    plt.subplot(1,6,i+2)
+    plt.imshow(img)
+    plt.title(f"{similarities[idx]:.2f}")
+    plt.axis("off")
+
+plt.tight_layout()
+plt.show()
